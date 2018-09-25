49f199e2bc22d363a3c4d168df8672d6377b5050
Hernandez <javier.hernandez@chubb.com>
2018-09-25 09:41:39 -0500

    Las imagenes

---
+++ Ferreteria/Models/context.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data.Entity;
+using System.Data.Entity.ModelConfiguration.Conventions;
+using System.Linq;
+using System.Web;
+
+namespace Ferreteria.Models
+{
+    public class context: DbContext
+    {
+        public DbSet<producto> products { get; set; }
+       
+
+        protected override void OnModelCreating(DbModelBuilder modelBuilder)
+        {
+            modelBuilder.Conventions.Remove<PluralizingTableNameConvention>();
+        }
+    }
+}
---
+++ Ferreteria/Models/producto.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace Ferreteria.Models
+{
+    public class producto
+    {
+        public int productoId { get; set; }
+        public int categoriaId { get; set; }
+        public decimal precio { get; set; }
+        public int cantidad { get; set; }
+    }
+}
